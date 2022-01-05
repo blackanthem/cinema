d328e496be2ec2c95b29ae9bf6e50e570b13a051
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-01-05 00:14:02 +0000

    remove listener on unmount

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,15 +1,23 @@
 import "./NavBar.scss";
 import logo from "../../assets/images/logo.svg";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function NavBar() {
   const [opaque, setOpaque] = useState(false);
+  const { pathname } = useLocation();
+
+  const scrollListener = () => {
+    if (window.scrollY < 200) setOpaque(false);
+    else setOpaque(true);
+  };
 
   useEffect(() => {
-    document.addEventListener("scroll", () => {
-      if (window.scrollY < 200) setOpaque(false);
-      else setOpaque(true);
-    });
+    if (pathname !== "/") return setOpaque(false);
+
+    document.addEventListener("scroll", scrollListener);
+
+    return () => document.removeEventListener("scroll", scrollListener);
   });
 
   const className = () => {
