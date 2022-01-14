fef7c4d5f930189dd5f8dbe14b3c3fb1a314d46c
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-01-14 16:49:59 +0000

    redirect routes to react router

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,20 @@ import express from "express";
 import { router } from "./routes/routes";
 import { authRouter } from "./routes/authRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
+import { join } from "path";
 
 const app = express();
 
-app.use(express.static("./client/build"));
+app.use(express.static(join(__dirname, "../client/build")));
+
 app.use(express.json());
 app.use("/v1", router);
 app.use("/v1/auth", authRouter);
 
 app.use(errorHandler);
+
+app.get("/*", (req, res) => {
+  res.sendFile(join(__dirname, "../client/build/index.html"));
+});
 
 export default app;
