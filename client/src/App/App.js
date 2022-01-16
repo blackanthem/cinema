67c1be68bd880c3ef9d532f7dd9ebaaa4445c5de
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-01-16 18:32:20 +0000

    delete breakpointSlice

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -3,24 +3,13 @@ import "./App.scss";
 import NavBar from "../components/NavBar/NavBar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { getWindowSize } from "../store/breakpointSlice";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    window.onload = window.onresize = () => {
-      dispatch(getWindowSize());
-    };
-  }, []);
-
   return (
     <div className="App">
       <NavBar />
       <ToastContainer
-        position="top-right"
+        position="bottom-right"
         autoClose={5000}
         hideProgressBar={true}
         newestOnTop={false}
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 // import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { api } from "../services/api";
 import authReducer from "./authSlice";
-import breakpointReducer from "./breakpointSlice";
 
 import { loginMiddleware } from "./middleware.js/loginMiddleware";
 
@@ -10,7 +9,6 @@ export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth: authReducer,
-    bp: breakpointReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware).concat(loginMiddleware),
