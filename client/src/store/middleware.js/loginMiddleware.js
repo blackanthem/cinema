65cdee8c0e115b23041343befbb511e629236b1d
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-01-16 17:00:34 +0000

    session expire toast in middleware

--- client/src/store/middleware.js/loginMiddleware.js
+++ client/src/store/middleware.js/loginMiddleware.js
@@ -1,10 +1,12 @@
 import { logout } from "../authSlice";
+import { toast } from "react-toastify";
 
 export const loginMiddleware =
   ({ dispatch }) =>
   (next) =>
   (action) => {
     if (action?.payload?.originalStatus === 401) {
+      toast.error("Session expired", {});
       dispatch(logout());
     }
 
